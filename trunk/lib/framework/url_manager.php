e4f2bcaedbc1eb0999620c25f2f24b3cda23dc5c
Cody B. Daig <cody@daig.me>
2014-02-11 17:24:38 -0700

    Initial URL Manager Class

---
+++ trunk/lib/framework/url_manager.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * This page will handle the urls, domains, sub domains, and slugs for the website.
+ *
+ * @author codydaig
+ */
+class URL {
+  private $DOMAIN;
+  private $SUB_DOMAIN;
+  
+  function __construct()
+  {
+    
+  }
+}
