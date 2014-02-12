b96aa9cc5b08bb7f290ed73815cc5500864ef0fa
Cody B. Daig <cody@daig.me>
2014-02-11 17:22:13 -0700

    Sample Config File

---
+++ trunk/lib/config.sample.php
@@ -0,0 +1,10 @@
+<?php
+
+// Config and Vars for Entire Site
+
+$SQLHOST = 'localhost';
+$SQLUSER = 'user';
+$SQLPASS = 'pass';
+$SQLDB = 'db_name';
+
+$BACKEND_SLUG = 'backend';  // You can change this to be unique at any time, but 301s will not be created.
