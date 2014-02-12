6ebfb92671b4888249cce2d6c471e0debe19b5bd
Cody B. Daig <cody@daig.me>
2014-02-11 22:14:10 -0700

    Created Page Class

---
+++ trunk/lib/framework/page_manager.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Description of page_manager
+ *
+ * @author codydaig
+ */
+class PAGE extends Framework{
+  private $url;
+  
+  function __construct($url)
+  {
+    $this->url=$url;
+  }
+}
