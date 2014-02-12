7d84b06616c2bdadbda38fc4913abc4039d8c381
Cody B. Daig <cody@daig.me>
2014-02-11 18:37:19 -0700

    Reduced index.php jobs

--- trunk/content/index.php
+++ trunk/content/index.php
@@ -2,19 +2,13 @@
 
 // Welcome to Cody's PHP Framework
 
-// Include the Config File
-require_once('../globals/config.php');
+// Include the Magic Loader (bootstrap) File
+require_once('../lib/bootstrap.php');
 
-// Connect to the Database
-mysqli_connect($SQLHOST, $SQLUSER, $SQLPASS, $SQLDB);
+// var_dump($_SERVER);
 
-// Check Database Connection
-if (mysqli_connect_errno())
-{
-  print "Failed to connect to MySQL: " . mysqli_connect_error();
-  die();
-}
 
+/*
 // Determine the URL being requested
 define('URL', $_SERVER["REQUEST_URI"]);
 $url_exploded = explode("/", URL);
@@ -33,5 +27,5 @@ else
 
 // What Page is being requested (slug manager)
 var_dump($_SERVER);
-
+*/
 ?>
