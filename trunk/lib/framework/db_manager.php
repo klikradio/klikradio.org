ce0dee143774c65db4bab248c22225ac77e0fcc5
Cody B. Daig <cody@daig.me>
2014-02-11 18:37:32 -0700

    Created Basic Database Manager (queries only)

--- trunk/lib/framework/db_manager.php
+++ trunk/lib/framework/db_manager.php
@@ -1,16 +1,62 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- * Description of db
+ * Handle All Interactions with the Database
  *
  * @author codydaig
  */
 class DB {
-  //put your code here
+  private $conn;
+  
+  private $host;
+  private $user;
+  private $pass;
+  private $name;
+  private $persistent;
+  
+  private $query_count;
+  
+  // Connect to Database
+  function __construct($SQLHOST, $SQLUSER, $SQLPASS, $SQLDB, $PERSISTENT=false)
+  {
+    $this->host = $SQLHOST;
+    $this->user = $SQLUSER;
+    $this->pass = $SQLPASS;
+    $this->name = $SQLDB;
+    $this->persistent = $PERSISTENT;
+    
+    // Connect to the Database
+    if($PERSISTENT)
+    {
+      $this->conn = mysqli_pconnect($this->host, $this->user, $this->pass, $this->name);
+    }
+    else
+    {
+      $this->conn = mysqli_connect($this->host, $this->user, $this->pass, $this->name);
+    }
+
+    // Check Database Connection
+    if (mysqli_connect_errno())
+    {
+      print "Failed to connect to MySQL: " . mysqli_connect_error();
+      die();
+    }
+  }
+  
+  // Query the Database
+  function query($sql)
+  {
+    $this->query_count++;
+    $query=mysqli_query($this->conn, $sql);
+    if($query===FALSE)
+    {
+      // QUERY FAILED
+    }
+    return $query;
+  }
+  
+  function get_query_count()
+  {
+    return $this->query_count;
+  }
 }
