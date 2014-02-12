fbf73dbf6242983ea0f902dff8158544be435589
Cody B. Daig <cody@daig.me>
2014-02-11 21:49:17 -0700

    Removed Old Stuff From Index

--- trunk/content/index.php
+++ trunk/content/index.php
@@ -6,27 +6,4 @@ print "<pre>";
 // Include the Magic Loader (bootstrap) File
 require_once('../lib/bootstrap.php');
 
-// var_dump($_SERVER);
-
-
-/*
-// Determine the URL being requested
-define('URL', $_SERVER["REQUEST_URI"]);
-$url_exploded = explode("/", URL);
-// Determine if Backend is being requested
-if($BACKEND_SLUG == $url_exploded[1])
-{
-  // Backend Requested
-  $slug = $url_exploded[2];
-}
-else
-{
-  $slug = $url_exploded[1];
-}
-
-// Check if User is Logged In Or Not && Has Access
-
-// What Page is being requested (slug manager)
-var_dump($_SERVER);
-*/
 ?>
