ca9bf5abe56b5190e47570db39b10af8ef28893b
Cody B. Daig <cody@daig.me>
2014-02-12 00:57:21 -0700

    Basic Page Manager

--- trunk/lib/framework/page_manager.php
+++ trunk/lib/framework/page_manager.php
@@ -8,8 +8,44 @@
 class PAGE extends Framework{
   private $url;
   
+  private $head=array();
+  private $content=array();
+  
   function __construct($url)
   {
     $this->url=$url;
+  }
+  
+  function add_head($line)
+  {
+    array_push($this->head, $line);
+  }
+  
+  function add_content($line)
+  {
+    array_push($this->content, $line);
+  }
+  
+  function get_head()
+  {
+    return $this->head;
+  }
+  
+  function get_content()
+  {
+    return $this->content;
+  }
+  
+  function add_element($element)
+  {
+    require_once(LIB_ROOT . '/pages/elements/' . $element . ".php");
+    new $element($this);
+  }
+  
+  function build_page()
+  {
+    $template = $this->url->get_template();
+    require_once(LIB_ROOT . '/pages/templates/' . $template . '.php');
+    new $template($this);
   }
 }
